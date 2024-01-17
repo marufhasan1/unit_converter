81331daca0e019bc533638c5a4f9348a886ac5e3
MD Maruf Hasan <emarufhasan@gmail.com>
2024-01-17 15:23:15 +0600

    Updated

--- test/index.php
+++ test/index.php
@@ -1,21 +1,25 @@
 <?php
-require __DIR__."/UnitConverter.php";
+require __DIR__ . "/UnitConverter.php";
 
 const rel = [
-        [
-            "caption" => "Cartoon",
-            "value" => 1
-        ],
-        [
-            "caption" => "Box",
-            "value" => 5
-        ],
-        [
-            "caption" => "KG",
-            "value" => 15
-        ],
-    ];
-    
-    $uc = new UnitConverter(rel);
-    print_r($uc->getRootValue([1,4,14]));
-    echo json_encode($uc->getUnitValue(76));
+    [
+        "caption" => "Cartoon",
+        "value" => 1
+    ],
+    [
+        "caption" => "Box",
+        "value" => 5
+    ],
+    [
+        "caption" => "KG",
+        "value" => 15
+    ],
+];
+
+//Each Cartoon contain 5 Box and Each Box contain 15 KG
+
+$uc = new UnitConverter(rel);
+
+print_r($uc->getRootValue([1, 4, 14]));
+echo "<hr/>";
+print_r($uc->getUnitValue(76));
